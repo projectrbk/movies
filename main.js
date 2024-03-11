70ea4af7947381c50a650ca6b8700f84de238262
brushurteeth8 <adem.hameed.123@gmail.com>
2024-03-11 14:56:09 +0100

    localstorage not fixed

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
-$('.submit').click(function () {
-    var movie = $("")
-})
+$("button").click(function () {
+  $(".movie").remove();
+});
+$(".submit").click(function () {
+  var name = $("#name").val();
+  var img = $("#URL").val();
+  var description = $("#des").val();
+
+
+  localStorage.setItem("name", name);
+  localStorage.setItem("img", img);
+  localStorage.setItem("description", description);
+
+  let movie = localStorage.getItem("name");
+  let photo = localStorage.getItem("img");
+  let dess = localStorage.getItem("description");
+  console.log(movie);
+  $(".container").append(`
+        <div class="movie">
+          <h3>${movie}</h3>
+          <img src="${photo}" alt="">
+          <p>${dess}</p>
+        </div>`);
+});
+
+console.log(localStorage);
